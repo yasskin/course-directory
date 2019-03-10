0653b5a713ed13e2d3795a3024b900330cdbc5b1
Noah Yasskin <noahyasskin@gmail.com>
2019-03-09 19:18:57 -0800

    Updates

---
+++ precache-manifest.63c2a09f02bad2c2b2c040fd724e3909.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbb7b8ed2fb062ca1beb",
+    "url": "/yasskin/course-directory/static/js/runtime~main.dbb7b8ed.js"
+  },
+  {
+    "revision": "b97d35275eb544529e10",
+    "url": "/yasskin/course-directory/static/js/main.b97d3527.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/yasskin/course-directory/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "b97d35275eb544529e10",
+    "url": "/yasskin/course-directory/static/css/main.c0bb42ce.chunk.css"
+  },
+  {
+    "revision": "cdc532c7b35255d5612f1536b681c99f",
+    "url": "/yasskin/course-directory/index.html"
+  }
+];
